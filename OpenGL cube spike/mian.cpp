aef11d3b13871184b8f79f216f03962e472c4ae0
Mateusz Łukomski <matrix.disc@gmail.com>
2015-08-24 09:04:00 +0200

    Using a texture, not displaying correctly yet

--- OpenGL cube spike/mian.cpp
+++ OpenGL cube spike/mian.cpp
@@ -5,23 +5,29 @@
 #include <SDL/SDL_opengl.h>
 #include <forward_list>
 #include <chrono>
+#include <SOIL/SOIL.h>
 
 // Shader sources
 const GLchar* vertexSource =
 "#version 150 core\n"
 "in vec2 position;"
 "in vec3 color;"
+"in vec2 texcoord;"
 "out vec3 Color;"
+"out vec2 Texcoord;"
 "void main() {"
 "	Color = color;"
+"	Texcoord = texcoord;"
 "	gl_Position = vec4(position, 0.0, 1.0);"
 "}";
 const GLchar* fragmentSource =
 "#version 150 core\n"
 "in vec3 Color;"
+"in vec2 Texcoord;"
 "out vec4 outColor;"
+"uniform sampler2D tex;"
 "void main() {"
-"	outColor = vec4(Color, 1.0);"
+"	outColor = texture(tex, Texcoord) * vec4(Color, 1.0);"
 "}";
 
 int main(int argc, char *argv[])
@@ -39,15 +45,24 @@ int main(int argc, char *argv[])
 	SDL_Event windowEvent;
 
 	float vertices[] = {
-		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, // Top-left
-		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, // Top-right
-		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, // Bottom-right
-		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f  // Bottom-left
+		//  Position      Color             Texcoords
+		-0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
+		0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
+		0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
+		-0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
 	};
 	GLuint elements[] = {
 		0, 1, 2,
 		2, 3, 0
 	};
+		
+	int width, height;
+	unsigned char* image = SOIL_load_image("img.png", &width, &height, 0, SOIL_LOAD_RGB);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+	SOIL_free_image_data(image);
+	
+	GLuint tex;
+	glGenTextures(1, &tex);
 	GLuint ebo;
 	glGenBuffers(1, &ebo);
 	GLuint vao;
@@ -59,6 +74,7 @@ int main(int argc, char *argv[])
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+	glBindTexture(GL_TEXTURE_2D, tex);
 
 	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexSource, NULL);
@@ -74,10 +90,13 @@ int main(int argc, char *argv[])
 	glUseProgram(shaderProgram);
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
 	GLint colorAttrib = glGetAttribLocation(shaderProgram, "color");
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
+	GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), 0);
 	glEnableVertexAttribArray(posAttrib);
-	glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
+	glVertexAttribPointer(colorAttrib, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), reinterpret_cast<void*>(2 * sizeof(float)));
 	glEnableVertexAttribArray(colorAttrib);
+	glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(float), reinterpret_cast<void*>(5 * sizeof(float)));
+	glEnableVertexAttribArray(texAttrib);
 
 	while (true)
 	{
