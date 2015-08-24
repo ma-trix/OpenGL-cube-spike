200ae67f37c0d1c975014aa1ed759d0411de1ca7
Mateusz Łukomski <matrix.disc@gmail.com>
2015-08-24 07:48:34 +0200

    Rendered white triangle

--- OpenGL cube spike/mian.cpp
+++ OpenGL cube spike/mian.cpp
@@ -3,6 +3,36 @@
 #include <GL/glew.h>
 #include <SDL/SDL.h>
 #include <SDL/SDL_opengl.h>
+#include <forward_list>
+
+// Shader sources
+const GLchar* vertexSource =
+"#version 150 core\n"
+"in vec2 position;"
+/*"in vec3 color;"
+"in vec2 texcoord;"
+"out vec3 Color;"
+"out vec2 Texcoord;"
+"uniform mat4 model;"
+"uniform mat4 view;"
+"uniform mat4 proj;"*/
+"void main() {"
+/*"   Color = color;"
+"   Texcoord = texcoord;"
+"   gl_Position = proj * view * model * vec4(position, 0.0, 1.0);"*/
+"	gl_Position = vec4(position, 0.0, 1.0);"
+"}";
+const GLchar* fragmentSource =
+"#version 150 core\n"
+/*"in vec3 Color;"
+"in vec2 Texcoord;"*/
+"out vec4 outColor;"
+/*"uniform sampler2D texKitten;"
+"uniform sampler2D texPuppy;"*/
+"void main() {"
+/*"   outColor = mix(texture(texKitten, Texcoord), texture(texPuppy, Texcoord), 0.5);"*/
+"	outColor = vec4(1.0, 1.0, 1.0, 1.0);"
+"}";
 
 int main(int argc, char *argv[])
 {
@@ -14,9 +44,42 @@ int main(int argc, char *argv[])
 
 	SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
 	SDL_GLContext context = SDL_GL_CreateContext(window);
+	glewExperimental = GL_TRUE;
+	glewInit();
 	SDL_Event windowEvent;
+
+	float vertices[] = {
+		0.0f,  0.5f, // Vertex 1 (X, Y)
+		0.5f, -0.5f, // Vertex 2 (X, Y)
+		-0.5f, -0.5f  // Vertex 3 (X, Y)
+	};
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	GLuint vbo;
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexSource, NULL);
+	glCompileShader(vertexShader);
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+	glCompileShader(fragmentShader);
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glBindFragDataLocation(shaderProgram, 0, "outColor");
+	glLinkProgram(shaderProgram);
+	glUseProgram(shaderProgram);
+	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(posAttrib);
+	
 	while (true)
 	{
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		if (SDL_PollEvent(&windowEvent))
 		{
 			if (windowEvent.type == SDL_QUIT) break;
